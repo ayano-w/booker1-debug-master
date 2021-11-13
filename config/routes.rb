3263559f1521b94fa75deafa35aaf6fa7645401b
ayano-w <ayasaku.t5@gmail.com>
2021-11-13 08:58:52 +0000

    [Add]divise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :bbbs, class_name: "Aaa::Bbb"
+  devise_for :users, class_name: "Public::User"
+  devise_for :public_users
+  devise_for :admin_users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   resources :books
---
+++ test/models/aaa/bbb_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Aaa::BbbTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
